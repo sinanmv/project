ee441fd29481b164135fc1b76c56e93738c6a77f
sinanmv <sinanmv234@gmail.com>
2023-10-20 13:10:28 +0530

    uaeproject

--- src/App.js
+++ src/App.js
@@ -1,25 +1,253 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import './App.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="wrapper">
+      <section>
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <div class="banner">
+                <div id="demo" class="carousel slide" data-bs-ride="carousel">
+                  <div class="carousel-inner">
+
+
+                    <div class="carousel-item active">
+                      <img src="https://info.bizzupcard.ae/uploads/banner/650_744_banner_20230718034124.png" alt="" class="d-block" style={{ width: "100%" }} />
+                    </div>
+
+                  </div>
+                  <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon"></span>
+                  </button>
+                  <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+                    <span class="carousel-control-next-icon"></span>
+                  </button>
+
+                </div>
+
+
+
+
+              </div>
+            </div>
+          </div>
+          <div class="row  mx-0 " style={{ marginTop: "-65px", paddingBottom: "40px" }}>
+            <div class="col-5">
+              <div class="logo ms-2">
+                <img src="https://info.bizzupcard.ae/uploads/customer/744_customer_logo_20230718034025.png" class="img-fluid" alt="" />
+              </div>
+            </div>
+            <div class="col-7">
+              <div class="mainName">
+                <span>KOTTAKKAL AYURVEDIC MEDICAL CENTER</span>
+                Ajman, United Arab Emirates                </div>
+            </div>
+          </div>
+        </div></section>
+
+      <div class="bg1 pt-5 mb-4" style={{ borderRadius: "20px" }}>
+        <section>
+          <div class="container">
+            <div class="row justify-content-center iconMain">
+              <div class="col-3">
+                <a href="https://www.youtube.com/@kottakkalayurvedauae4913"><i class="fa-brands fa-youtube"></i></a>
+              </div>
+
+              <div class="col-3">
+                <a href="https://twitter.com/ayurdubai"><i class="fa-brands fa-twitter"></i></a>
+              </div>
+
+              <div class="col-3">
+                <a href="https://www.instagram.com/kottakkal.ayurveda/"><i class="fa-brands fa-instagram"></i></a>
+              </div>
+
+              <div class="col-3">
+                <a href="https://www.facebook.com/kottakkalayurvedacenter"><i class="fa-brands fa-facebook-f"></i></a>
+              </div>
+
+              <div class="col-3">
+                <a href="https://www.linkedin.com/in/kottakkal-ayurveda-a657b3180/"><i class="fa-brands fa-linkedin"></i></a>
+              </div>
+
+
+
+              <div class="col-3">
+                <a href="tel:+971526474941"><i class="fa-solid fa-phone-volume"></i></a>
+              </div>
+              <div class="col-3">
+                <a href="mailto:info@kottakkalayurveda.ae"><i class="fa-solid fa-envelope"></i></a>
+              </div>
+              <div class="col-3">
+                <a href="https://wa.me/+971526474941?text=Hi" target="_blank"><i class="fa-brands fa-whatsapp"></i></a>
+              </div>
+
+              <div class="col-3">
+                <a href="https://www.kottakkalayurveda.ae/" target="_blank"><i class="fa-solid fa-globe"></i></a>
+              </div>
+              <div class="col-3">
+                <a href="https://g.page/r/CUWwRAOqK64TEBM/review" target="_blank"><img src="https://info.bizzupcard.ae/assets/official-2/images/icon/google.png" alt="" /> </a>
+              </div>
+
+              <div class="col-3">
+                <a href="https://goo.gl/maps/jApB2sSQbNyu28Lu8" target="_blank"><i class="fa-solid fa-location-dot"></i></a>
+              </div>
+              <div class="row justify-content-center">
+                <div class="col-auto my-1 p-1">
+                  <a href="https://info.bizzupcard.ae/uploads/customer/kottakkalayurvedic.vcf" download="" class="text-decoration-none button1 m-auto d-table">
+                    <i class="fa-solid fa-floppy-disk"></i>SAVE CONTACT
+                  </a>
+                </div>
+                <div class="col-auto my-1 p-1">
+                  <a href="" class="text-decoration-none button1 m-auto d-table" data-bs-toggle="modal" data-bs-target="#share-model">
+                    <i class="fa-sharp fa-solid fa-share-nodes"></i>SHARE
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+
+        <section>
+          <div class="container">
+            <div class="row">
+              <div class="col titleMain ">
+                <i class="fa-solid fa-user"></i>CONTACT US
+              </div>
+            </div>
+            <div class="row">
+              <div class="contMain">
+                <div class="contSub">
+                  <i class="fa-solid fa-phone"></i>
+                  <div class="contDis">
+                    <span>Primary</span><a href="tel:+971526474941" className='text-decoration-none'>+971526474941</a>                                                                <span>Primary</span><a href="tel:+97167426664">+97167426664</a>                                                            </div>
+                </div>
+                <div class="contSub">
+                  <i class="fa-brands fa-whatsapp"></i>
+                  <div class="contDis">
+                    <span>Primary</span><a href="https://wa.me/+971526474941?text=Hi" target="_blank">+971526474941</a>                                                        </div>
+                </div>
+                <div class="contSub">
+                  <i class="fa-solid fa-envelope"></i>
+                  <div class="contDis">
+                    <span>Primary</span><a href="mailto:info@kottakkalayurveda.ae">info@kottakkalayurveda.ae</a>
+                  </div>
+                </div>
+                <div class="contSub">
+                  <i class="fa-solid fa-globe"></i>
+                  <div class="contDis">
+                    <span>Primary</span><a href="https://www.kottakkalayurveda.ae/" target="_blank">www.kottakkalayurveda.ae/</a>                                                            </div>
+                </div>
+                <div class="contSub">
+                  <i class="fa-solid fa-location-dot"></i>
+                  <div class="contDis">
+                    Flat 106, Al Farah Plaza Building, Opp Mashreq Bank - Al Nakhil 1 , Ajman, United Arab Emirates                            </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div class="container">
+            <div class="row">
+              <div class="col titleMain">
+                <i class="fa-solid fa-users"></i>ABOUT US
+              </div>
+            </div>
+            <div class="row">
+              <div class="contMain">
+                <div class="contSub" style={{ display: "block" }}><p>A Natural Approach to Better Health to Look Good, Feel Better &amp; Live Longer | Kottakkal Ayurvedic Treatment Centre offers an extensive menu of therapeutic and rejuvenating Ayurvedic treatments that are result-oriented and designed to meet individual needs and health goals.<br /></p></div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div class="container">
+            <div class="row">
+              <div class="col titleMain">
+                <i class="fa-solid fa-hand-point-right"></i>OUR SERVICE
+              </div>
+            </div>
+            <div class="row">
+              <div class="contMain">
+                <ul>
+                  <li>Ayurvedic Consultation</li>
+                  <li>Ayurvedic Medicine</li>
+                  <li>Ayurveda Massage Therapy</li>
+                  <li>Yoga</li>
+                  <li>Physiotherapy Treatments</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div class="container">
+            <div class="row">
+              <div class="col titleMain">
+                <i class="fa-solid fa-building-columns"></i>BRANCH
+              </div>
+            </div>
+            <div class="row">
+              <div class="contMain">
+                <div class="row">
+                  <div class="col-8 pt-2">
+                    <div class="contSub">
+                      <div class="contDis list">
+                        Deira                                     </div>
+                    </div>
+                  </div>
+                  <div class="col-4">
+                    <a href="" target="_blank" class="text-decoration-none button1 m-auto d-table">
+                      <i class="fa-solid fa-list"></i>View</a>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-8 pt-2">
+                    <div class="contSub">
+                      <div class="contDis list">
+                        Dubai,Satwa                                    </div>
+                    </div>
+                  </div>
+                  <div class="col-4">
+                    <a href="" target="_blank" class="text-decoration-none button1 m-auto d-table">
+                      <i class="fa-solid fa-list"></i>View</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+
+
+
+
+        <footer>
+          <div class="container" style={{ marginTop: "30px", marginLeft: "-20px", marginRight: "-20px", width: "calc(100% + 40px)" }}>
+            <div class="container" style={{ marginTop: "30px", marginLeft: "-20px", marginRight: "-20px", width: "calc(100% + 40px)" }}>
+              <div class="row contactIcon">
+                <div class="col"><a href="tel:+971526474941"><i class="fa-solid fa-phone-volume"></i>Call</a></div>
+                <div class="col"><a href="https://wa.me/+971526474941?text=Hi"><i class="fa-brands fa-whatsapp"></i>Whatsapp</a></div>
+                <div class="col"><a href="https://goo.gl/maps/jApB2sSQbNyu28Lu8" target="_blank"><i class="fa-solid fa-location-dot"></i>Direction</a></div>
+                <div class="col"><a href="#" data-bs-toggle="modal" data-bs-target="#qr-modal"><i class="fa-solid fa-qrcode"></i>QR Code</a></div>
+              </div>
+            </div>
+
+
+          </div></footer>
+
+
+
+      </div>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
